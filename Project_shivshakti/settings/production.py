d191277399209ded10dc820581b79e70d506c250
Sangram Yadav <146555501+sangramyadav05@users.noreply.github.com>
2026-02-17 09:50:16 +0530

    Update production DB config for Render deployment

--- Project_shivshakti/settings/production.py
+++ Project_shivshakti/settings/production.py
@@ -1,30 +1,26 @@
-from django.core.exceptions import ImproperlyConfigured
 from .base import *
+from decouple import config
+import dj_database_url
 
 DEBUG = False
 
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="*"
+).split(",")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': env('POSTGRES_DB', 'shivshakti'),
-        'USER': env('POSTGRES_USER', 'shivshakti_user'),
-        'PASSWORD': env('POSTGRES_PASSWORD', ''),
-        'HOST': env('POSTGRES_HOST', 'localhost'),
-        'PORT': env('POSTGRES_PORT', '5432'),
-        'CONN_MAX_AGE': env_int('POSTGRES_CONN_MAX_AGE', 60),
-        'OPTIONS': {
-            'sslmode': env('POSTGRES_SSLMODE', 'require'),
-        },
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=60,
+        ssl_require=True
+    )
 }
 
-if DATABASES['default']['OPTIONS'].get('sslmode') != 'require':
-    raise ImproperlyConfigured('POSTGRES_SSLMODE must be set to "require" in production.')
-
-SECURE_SSL_REDIRECT = env_bool('DJANGO_SECURE_SSL_REDIRECT', True)
+SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-SECURE_HSTS_SECONDS = env_int('DJANGO_SECURE_HSTS_SECONDS', 31536000)
+SECURE_HSTS_SECONDS = 31536000
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
